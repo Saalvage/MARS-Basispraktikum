c9e1a02f96123e56b7bc3732d58bb43ab4283521
veropossumus <16821784+veropossumus@users.noreply.github.com>
2023-05-17 13:46:16 +0200

    implement interpolate_cubic_periodic and unit circle interpolation

--- src/cagd/spline.py
+++ src/cagd/spline.py
@@ -232,6 +232,27 @@ class spline:
     # of a periodic spline with equidistant knots
     # returns that spline object
     def interpolate_cubic_periodic(points):
+
+        m = len(points)-1
+        degree = 3
+        spline_obj = spline(degree)
+        spline_obj.knots = knots(1)
+        spline_obj.knots.knots = list(range(-degree-1, m+degree+1))
+
+        print("spline_obj.knots.knots:")
+        for knot in spline_obj.knots.knots:
+            print(knot)
+
+        diag1 = diag3 = [1 / 6] * (m+1)
+        diag2 = [4 / 6] * (m+1)
+
+        spline_obj.control_points = utils.solve_almost_tridiagonal_equation(diag1, diag2, diag3, points)
+
+        for i in range(degree):
+            spline_obj.control_points.append(spline_obj.control_points[i])
+
+        return spline_obj
+
         pass
 
     # for splines of degree 3, generate a parallel spline with distance dist
--- src/periodic-interpolation.py
+++ src/periodic-interpolation.py
@@ -1,20 +1,41 @@
 #!/usr/bin/python
+import math
 
 import cagd.scene_2d as scene
 from cagd.vec import vec2
 from cagd.spline import spline, knots
 from cagd.polyline import polyline
 
-#returns a list of num_samples points that are uniformly distributed on the unit circle
-def unit_circle_points(num_samples):
-    return []
 
-#calculates the deviation between the given spline and a unit circle
+# returns a list of num_samples points that are uniformly distributed on the unit circle
+def unit_circle_points(num_samples):
+    section_length = 2 * math.pi / num_samples
+    angles = [i * section_length for i in range(num_samples)]
+
+    points = []
+    for angle in angles:
+        points.append(vec2(math.cos(angle), math.sin(angle)))
+    return points
+
+
+# calculates the deviation between the given spline and a unit circle
 def calculate_circle_deviation(spline):
+    deviations = []
+    control = spline.control_points
+    for i in range(len(control)):
+        deviation = math.sqrt(control[i].x ** 2 + control[i].y ** 2) - 1  # should be 0 in a perfect circle
+        deviations.append(deviation)
+
+    mean_error = sum(deviations) / len(deviations)
+    print("mean error:", mean_error)
+    max_error = max(deviations)
+    print("maximum error:", max_error)
+
     pass
 
-#interpolate 6 points with a periodic spline to create the number "8"
-pts = [vec2( 0, 2.5), vec2(-1, 1), vec2( 1,-1), vec2( 0,-2.5), vec2(-1,-1), vec2(1,1)]
+
+# interpolate 6 points with a periodic spline to create the number "8"
+pts = [vec2(0, 2.5), vec2(-1, 1), vec2(1, -1), vec2(0, -2.5), vec2(-1, -1), vec2(1, 1)]
 pts_line = polyline()
 pts_line.points = pts
 pts_line.set_color("red")
@@ -26,12 +47,12 @@ sc.set_resolution(900)
 sc.add_element(s)
 sc.add_element(p)
 
-#generate a spline that approximates the unit circle
-#n = 8
-#circle_pts = unit_circle_points(n)
-#circle = spline.interpolate_cubic_periodic(circle_pts)
-#sc.add_element(circle)
-#calculate_circle_deviation(circle)
+# generate a spline that approximates the unit circle
+n = 8
+circle_pts = unit_circle_points(n)
+circle = spline.interpolate_cubic_periodic(circle_pts)
+sc.add_element(circle)
+calculate_circle_deviation(circle)
 
 sc.write_image()
 sc.show()
